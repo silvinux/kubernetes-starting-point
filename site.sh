22f5ec31ba7bc9eae0e0430bb69f9cfc57ae7073
silvinux <silvinux7@gmail.com>
2023-04-19 19:59:47 +0200

    First commit

--- site.sh
+++ site.sh
@@ -3,4 +3,5 @@
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 rm -rf $_CURR_DIR/gh-pages $_CURR_DIR/.cache
 
-antora --pull --stacktrace  site.yml
+# npx antora --pull --stacktrace  site.yml
+npx antora --stacktrace  site.yml
